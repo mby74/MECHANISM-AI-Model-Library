ea39e8ea8a1114c4d7e7b4c08c958959045756db
Majid Bani <114766106+mby74@users.noreply.github.com>
2026-03-29 20:29:36 -0500

    Create model.py

---
+++ mechanistic_models/signaling/bagaev2019_tlr4_nfkb_bmdm/src/model.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def simulate_nfkb(t, k_act, k_deg, S, B):
+    """
+    Simulate reduced NF-κB dynamics.
+
+    Parameters:
+        t : array of time points
+        k_act : activation rate
+        k_deg : degradation rate
+        S : stimulation strength
+        B : basal NF-κB level (initial condition)
+
+    Returns:
+        N : NF-κB nuclear timecourse
+    """
+
+    N = np.zeros_like(t)
+    N[0] = B
+
+    for i in range(1, len(t)):
+        dt = t[i] - t[i-1]
+        dN = k_act * (1 - N[i-1]) * S - k_deg * N[i-1]
+        N[i] = N[i-1] + dt * dN
+
+    return N
+
+
+def time_to_peak(t, N):
+    return t[np.argmax(N)]
